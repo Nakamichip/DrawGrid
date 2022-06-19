46399d32091b9eb8a2755f9d363ca56b9dd62f9f
NakamichiIT <nakamichip@gmail.com>
2022-06-19 13:53:03 +0300

    Commit Changes

--- src/ArrayExersise.java
+++ src/ArrayExersise.java
@@ -5,7 +5,7 @@ public class ArrayExersise {
     public static void main(String[] args) {
 
         findSecondLargestElementInArray(new Integer[]{1, 2, 3, 40, 25, 7, 19, 2, 4});
-        findSecondSmallestInArray(new Integer[]{22, 35, 48, 12, 63, 1, 31, 98, 65, 15});
+        findSecondSmallestInArray(new Integer[]{22, 99, 48, 12, 63, 1, 31, 98, 65, 15});
     }
 
     public static void findSecondLargestElementInArray(Integer[] array) {
